3bba7a15bd895a817a2f478179fef409050ffd88
panfio <45535760+panfio@users.noreply.github.com>
2019-12-17 16:45:41 +0300

    #1 Used lombok annotations instead plain java

--- telescreen/src/main/java/ru/panfio/telescreen/model/YouTube.java
+++ telescreen/src/main/java/ru/panfio/telescreen/model/YouTube.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.panfio.telescreen.util.IsoInstantLocalDateTimeDeserializer;
 import ru.panfio.telescreen.util.IsoInstantLocalDateTimeSerializer;
 
@@ -14,7 +16,8 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-
+@Data
+@NoArgsConstructor
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class YouTube {
@@ -28,70 +31,4 @@ public class YouTube {
     @JsonDeserialize(using = IsoInstantLocalDateTimeDeserializer.class)
     private LocalDateTime time;
 
-    public YouTube(Long id, String title, String url, LocalDateTime time) {
-        this.id = id;
-        this.title = title;
-        this.url = url;
-        this.time = time;
-    }
-
-    public YouTube() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public LocalDateTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalDateTime time) {
-        this.time = time;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        YouTube youTube = (YouTube) o;
-        return getTitle().equals(youTube.getTitle()) &&
-                getUrl().equals(youTube.getUrl()) &&
-                getTime().equals(youTube.getTime());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getTitle(), getUrl(), getTime());
-    }
-
-    @Override
-    public String toString() {
-        return "YouTube{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", url='" + url + '\'' +
-                ", time=" + time +
-                '}';
-    }
 }
