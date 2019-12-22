c8e84eed443beed65d25caaa1383db51bad65d89
panfio <45535760+panfio@users.noreply.github.com>
2019-12-22 20:06:38 +0300

    Refactoring scripts

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,26 +2,31 @@
 #set -x; 
 #This script copy files from android phone into archive folder
 
-
-ARCHIVE_FOLDER='/home/apoh/archive'
+ARCHIVE_FOLDER=$1
 
 MTP=$(ls /run/user/1000/gvfs)
 PHONE_STORAGE="/run/user/1000/gvfs/$MTP/Internal shared storage"
 
+copy() {
+#Params: from | to | mask
+find "$PHONE_STORAGE$1" -type f -name $3 | xargs -i cp -v -u {} $ARCHIVE_FOLDER$2
+}
+
 #Not require root
-find "$PHONE_STORAGE/DCIM/Camera" -type f -name "IMG_*.jpg" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/media/photo/
-find "$PHONE_STORAGE/DCIM/Camera" -type f -name "VID_*.mp4" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/media/video/
-find "$PHONE_STORAGE/EasyVoiceRecorder" -type f -name "*note.m4a" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/media/voicenote/
-find "$PHONE_STORAGE/Pictures/Screenshots" -type f -name "Screenshot*.png" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/media/screenshot/
-find "$PHONE_STORAGE'/Day One'" -type f -name "Export-Journal.zip" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/app/dayone/
-find "$PHONE_STORAGE/Timesheet" -type f -name "TimesheetBackup*.xml" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/app/timesheet/
+copy "/DCIM/Camera" "/media/photo/" "IMG_*.jpg"
+copy "/DCIM/Camera" "/media/video/" "VID_*.mp4" 
+copy "/EasyVoiceRecorder" "/media/voicenote/" "*note.m4a" 
+copy "/Pictures/Screenshots" "/media/screenshot/" "Screenshot*.png" 
+copy "/Day One" "/app/dayone/" "Export-Journal.zip" 
+copy "/Timesheet" "/app/timesheet/" "TimesheetBackup*.xml"
 
 # #root
-find "$PHONE_STORAGE/archive/app/soundcloud" -type f -name "*.db" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/app/soundcloud/
-find "$PHONE_STORAGE/archive/app/soundcloud" -type f -name "SoundCloud" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/app/soundcloud/
-find "$PHONE_STORAGE/archive/app/spotify" -type f -name "*.bnk" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/app/spotify/
-find "$PHONE_STORAGE/archive/app/sms" -type f -name "bugle_db" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/app/sms/
-find "$PHONE_STORAGE/archive/app/call" -type f -name "calllog.db" | xargs -i cp -v -u {} $ARCHIVE_FOLDER/app/call/
+copy "/archive/app/soundcloud" "/app/soundcloud/" "*.db" 
+copy "/archive/app/soundcloud" "/app/soundcloud/" "SoundCloud"
+copy "/archive/app/spotify" "/app/spotify/" "*.bnk"
+copy "/archive/app/sms" "/app/sms/" "bugle_db"
+copy "/archive/app/call" "/app/call/" "calllog.db"
+copy "/archive/app/wellbeing" "/app/wellbeing/" "app_usage"
 
 echo "===== Done ====="
 
